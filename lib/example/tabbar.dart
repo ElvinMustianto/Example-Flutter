e52eec9c9da2cd32d120b55c538912ee0e3d4ea8
ElvinMustianto <mustianto298@gmail.com>
2024-06-24 23:08:44 +0700

    update tabBar and new file QR Code

--- lib/example/tabbar.dart
+++ lib/example/tabbar.dart
@@ -5,33 +5,76 @@ class ExampleTabBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TabBar myTabBar = const TabBar(
+      indicator: BoxDecoration(
+          border:
+              Border(bottom: BorderSide(color: Colors.redAccent, width: 5))),
+      tabs: [
+        Tab(
+          icon: Icon(
+            Icons.comment,
+            color: Colors.white,
+          ),
+          child: Text(
+            "Comments",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        Tab(
+          icon: Icon(
+            Icons.home,
+            color: Colors.white,
+          ),
+          child: Text(
+            "Home",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        Tab(
+          icon: Icon(
+            Icons.contact_page,
+            color: Colors.white,
+          ),
+          child: Text(
+            "Contact",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        Tab(
+          icon: Icon(
+            Icons.account_box,
+            color: Colors.white,
+          ),
+          child: Text(
+            "Account",
+            style: TextStyle(color: Colors.white),
+          ),
+        )
+      ],
+    );
     return DefaultTabController(
         length: 4,
         child: Scaffold(
           appBar: AppBar(
-            title: const Text("Example TabBar", style: TextStyle(color: Colors.white),),
-            backgroundColor: Colors.blueAccent,
-            bottom: const TabBar(
-              tabs: [
-                Tab(
-                  icon: Icon(Icons.comment, color: Colors.white,),
-                  child: Text("Comments", style: TextStyle(color: Colors.white),),
+              title: const Text(
+                "Example TabBar",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+              ),
+              backgroundColor: Colors.blueAccent,
+              bottom: PreferredSize(
+                preferredSize: Size.fromHeight(myTabBar.preferredSize.height),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [Colors.redAccent, Colors.amberAccent],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: myTabBar,
                 ),
-                Tab(
-                  icon: Icon(Icons.home, color: Colors.white,),
-                  child: Text("Home", style: TextStyle(color: Colors.white),),
-                ),
-                Tab(
-                  icon: Icon(Icons.contact_page, color: Colors.white,),
-                  child: Text("Contact", style: TextStyle(color: Colors.white),),
-                ),
-                Tab(
-                  icon: Icon(Icons.account_box, color: Colors.white,),
-                  child: Text("Account", style: TextStyle(color: Colors.white),),
-                )
-              ],
-            ),
-          ),
+              )),
           body: const TabBarView(
             children: [
               Center(
@@ -48,7 +91,6 @@ class ExampleTabBar extends StatelessWidget {
               ),
             ],
           ),
-        )
-    );
+        ));
   }
 }
